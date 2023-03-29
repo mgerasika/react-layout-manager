3612a0879245b32682c4901a18e5aed86b911882
Mykhaylo Gerasika <mgerasika@gmail.com>
2023-03-29 16:55:12 +0300

    Remove build

--- dist/types/example/example-full.component.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const EViewName: {
-    JSON: string;
-    CodeEditor: string;
-    Terminal: string;
-    SolutionExplorer: string;
-    Questions: string;
-    Chat: string;
-};
-export declare function ExampleFull(): JSX.Element;
--- dist/types/lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const demo: () => JSX.Element;
-export { LayoutContext } from "./layout/layout-context.component";
-export { Layout } from "./layout/layout.component";
-export { useLayout } from "./layout/layout.hook";
